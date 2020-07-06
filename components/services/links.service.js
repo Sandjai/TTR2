4948fb517f4853fa061e0c4934cf6f9d935df9b9
Sandjai <photoplum@mail.ru>
2020-07-06 21:18:19 +0300

    update BASE_url link 2

--- components/services/links.service.js
+++ components/services/links.service.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
-const BASE_URL = 'https://components-2130.firebaseio.com/menu/-M-buribBxTNSjv-sJkM/-M-buYH2BkaQ_DE7FhuD.json';
+const BASE_URL = 'https://components-2130.firebaseio.com/menu/-M-buribBxTNSjv-sJkM.json';
 
 /** 
  * Data source for links collection
